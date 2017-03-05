e5ee36fb4b8e5f380d2fbd428f246c709bfb85cf
Phoenix-Z <phoenixzhao1105@gmail.com>
2017-03-05 17:57:36 +0800

    completation of id3 algorithm.

--- id3.py
+++ id3.py
@@ -1,10 +1,106 @@
 # coding:utf-8
-import math
+import operator
+from math import log
+
+# 构造数据集和属性集
+def createDataSet():
+    dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    features = ['no surfacing', 'flippers']
+    return dataSet, features
+
+# 生成决策树
+def treeGrowth(dataSet, features):
+    classList = [example[-1] for example in dataSet]
+    # 只有一个决策结果
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    # 只有一个属性的时候，不必再比较信息增益
+    if len(dataSet[0]) == 1:  # no more features
+        return classify(classList)
+
+    bestFeat = findBestSplit(dataSet)  # bestFeat is the index of best feature
+    bestFeatLabel = features[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueFeatValues = set(featValues)
+    del (features[bestFeat])
+    for values in uniqueFeatValues:
+        subDataSet = splitDataSet(dataSet, bestFeat, values)
+        myTree[bestFeatLabel][values] = treeGrowth(subDataSet, features)
+    return myTree
 
 
-def entropy(prob):
-    return -sum([prob[i] * math.log(prob[i], 2) for i in xrange(len(prob))])
+def classify(classList):
+    """
+    find the most in the set
+    """
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
 
-probabilities = [0.5] * 2
-print entropy(probabilities)
 
+def findBestSplit(dataset):
+    numFeatures = len(dataset[0]) - 1
+    baseEntropy = calcShannonEnt(dataset)
+    bestInfoGain = 0.0
+    bestFeat = -1
+    for i in range(numFeatures):
+        featValues = [example[i] for example in dataset]
+        uniqueFeatValues = set(featValues)
+        newEntropy = 0.0
+        for val in uniqueFeatValues:
+            subDataSet = splitDataSet(dataset, i, val)
+            prob = len(subDataSet) / float(len(dataset))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        if (baseEntropy - newEntropy) > bestInfoGain:
+            bestInfoGain = baseEntropy - newEntropy
+            bestFeat = i
+    return bestFeat
+
+
+def splitDataSet(dataset, feat, values):
+    retDataSet = []
+    for featVec in dataset:
+        if featVec[feat] == values:
+            reducedFeatVec = featVec[:feat]
+            reducedFeatVec.extend(featVec[feat + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def calcShannonEnt(dataset):
+    numEntries = len(dataset)
+    labelCounts = {}
+    for featVec in dataset:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        if prob != 0:
+            shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+def predict(tree, newObject):
+    while isinstance(tree, dict):
+        key = tree.keys()[0]
+        tree = tree[key][newObject[key]]
+    return tree
+
+
+if __name__ == '__main__':
+    dataset, features = createDataSet()
+    tree = treeGrowth(dataset, features)
+    print tree
+    print predict(tree, {'no surfacing': 1, 'flippers': 1})
+    print predict(tree, {'no surfacing': 1, 'flippers': 0})
+    print predict(tree, {'no surfacing': 0, 'flippers': 1})
+    print predict(tree, {'no surfacing': 0, 'flippers': 0})
